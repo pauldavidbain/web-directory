dbfa2242045215f2e75c1ef1a7a612d71108e0a9
Ryan Hall <ryan.hall@biola.edu>
2014-03-31 15:58:28 -0700

    For some reason the migration file that got created didn't have the .rb extension.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331205716) do
+ActiveRecord::Schema.define(version: 20140331225416) do
 
   create_table "assignments", force: true do |t|
     t.integer "human_id"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 20140331205716) do
   end
 
   add_index "roles", ["name"], name: "index_roles_on_name"
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.string   "cas_ticket"
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["cas_ticket"], name: "index_sessions_on_cas_ticket"
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
     t.string   "username"
