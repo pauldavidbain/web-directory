935a8073779b3248c670a537fb40733274291318
Ryan Hall <ryan.hall@biola.edu>
2014-05-15 10:39:38 -0700

    Updated biola_style_guide gem and added links to different apps. Updated font_awesome_rails.

--- config/initializers/biola_style_guide.rb
+++ config/initializers/biola_style_guide.rb
@@ -2,4 +2,9 @@ BiolaStyleGuide.configure do |config|
   config.app_name = Settings.app.name
   # config.app_version = Version.current  # Don't set this right now
   config.relative_root = Settings.app.relative_url_root
+  config.app_links = [
+    {title: Settings.app.name, url: Settings.app.relative_url_root, icon: 'home'},
+    {title: 'Gmail', url: 'http://mail.biola.edu', icon: 'envelope-square'},
+    {title: 'Forms', url: 'http://forms.biola.edu', icon: 'check-square-o'},
+  ]
 end
