86d6af5a96c46336defce5d051ba9d2c166d5b80
Ryan Hall <ryan.hall@biola.edu>
2014-06-17 17:12:44 -0700

    Updated policies.

--- app/policies/person_policy.rb
+++ app/policies/person_policy.rb
@@ -2,17 +2,17 @@ class PersonPolicy < ApplicationPolicy
   class Scope < Struct.new(:user, :scope)
     def resolve
       if user.nil?
-        scope.not_confidential.any_in(affiliations: [ 'trustee', 'faculty' ])
+        scope.any_in(visible_affiliations: [ 'trustee', 'faculty' ])
       elsif user.admin? || user.developer?
         scope.all
       elsif user.employee? || user.faculty?
-        scope.not_confidential.any_in(affiliations: [ 'employee', 'student', 'trustee', 'faculty', 'alumnus' ])
+        scope.any_in(visible_affiliations: [ 'employee', 'student', 'trustee', 'faculty', 'alumnus' ])
       elsif user.student?
-        scope.not_confidential.any_in(affiliations: [ 'employee', 'student', 'trustee', 'faculty' ])
+        scope.any_in(visible_affiliations: [ 'employee', 'student', 'trustee', 'faculty' ])
       elsif user.alumnus?
-        scope.not_confidential.any_in(affiliations: [ 'trustee', 'faculty', 'alumnus' ])
+        scope.any_in(visible_affiliations: [ 'trustee', 'faculty', 'alumnus' ])
       else
-        scope.not_confidential.any_in(affiliations: [ 'trustee', 'faculty' ])
+        scope.any_in(visible_affiliations: [ 'trustee', 'faculty' ])
       end
     end
   end
