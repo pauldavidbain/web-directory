c4ad26a8f5a28031a55a2e57d84d05b91f4723e2
Devin Hanlon <devin.m.hanlon@biola.edu>
2014-06-24 14:18:30 -0700

    Hotfix to pinglish and added elasticsearch ping

--- config/initializers/pinglish.rb
+++ config/initializers/pinglish.rb
@@ -1,6 +1,6 @@
 Directory::Application.config.middleware.use Pinglish do |ping|
-  ping.check :db do
-    ActiveRecord::Base.connected?
+  ping.check :mongodb do
+    Mongoid.default_session.command(ping: 1).has_key? 'ok'
   end
 
   ping.check :smtp do
@@ -10,5 +10,9 @@ Directory::Application.config.middleware.use Pinglish do |ping|
     smtp.finish
 
     ok
+  end
+
+  ping.check :elasticsearch do
+    Elasticsearch::Model.client.indices.exists index: Settings.elasticsearch.index_name
   end
 end
