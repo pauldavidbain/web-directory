05e751082ec3b896383784bd51f6fa1dc1757902
Ryan Hall <ryan.hall@biola.edu>
2014-08-22 09:50:39 -0700

    DIR-101 add biography to person profile Also added presenters to clean up the view code a little bit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,10 @@ class PeopleController < ApplicationController
   before_filter :pundit_authorize
 
   def show
+    @person_presenter = PersonPresenter.new(view_context, @person)
+    @bio_presenter = BiographyPresenter.new(view_context, @person.bio_edition)
+
+    @profile_img = @person.biola_photo_url || view_context.biola_person_image(@person.try(:biola_id), :large)
   end
 
 
---
+++ app/presenters/biography_presenter.rb
@@ -0,0 +1,2 @@
+class BiographyPresenter < ApplicationPresenter
+end
