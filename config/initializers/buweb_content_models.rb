3dde4dc7181d99e0ece90c5c5bfaa4fc3e23c4d7
Ryan Hall <ryan.hall@biola.edu>
2014-08-25 13:50:47 -0700

    Adding roz support and adding banner image and logo.

--- config/initializers/buweb_content_models.rb
+++ config/initializers/buweb_content_models.rb
@@ -2,4 +2,8 @@ BuwebContentModels.configure do |config|
   config.elasticsearch_host = Settings.elasticsearch.host
   config.elasticsearch_index = Settings.elasticsearch.index_name
   config.urn_namespaces = Settings.urn_namespaces
+  config.roz_api_base_url = Settings.roz.api_base_url
+  config.roz_files_base_url = Settings.roz.files_base_url
+  config.roz_access_id = Settings.roz.access_id
+  config.roz_secret_key = Settings.roz.secret_key
 end
