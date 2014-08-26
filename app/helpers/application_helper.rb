73939675f2d1225fe051ddd5268f01be04750187
Ryan Hall <ryan.hall@biola.edu>
2014-08-26 15:27:30 -0700

    Using primary photo for person on department and group pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 module ApplicationHelper
   def biola_person_image(netid, size=:medium)
     if netid.present?
-      digest = Digest::MD5.hexdigest(netid.to_s)
-      "https://apps.biola.edu/idphotos/#{digest}_#{size.to_s}.jpg"
+      if netid.is_a?(Person)
+        person = netid
+        person.profile_photo_url || netid_to_image(person.biola_id, size)
+      else
+        netid_to_image(netid, size)
+      end
     end
+  end
+
+  def netid_to_image(netid, size=:medium)
+    digest = Digest::MD5.hexdigest(netid.to_s)
+    "https://apps.biola.edu/idphotos/#{digest}_#{size.to_s}.jpg"
   end
 
   def divider(text=nil)
