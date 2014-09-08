94197832c2621dd9e45425aae19db30791e36315
Adam Crownoble <adam.crownoble@biola.edu>
2014-09-08 16:58:20 -0700

    Use Mongoid session store

--- config/initializers/rack-cas.rb
+++ config/initializers/rack-cas.rb
@@ -7,11 +7,11 @@ if Rails.env.test?
   Directory::Application.config.middleware.swap Rack::FakeCAS, Rack::FakeCAS
 else
   require 'rack/cas'
-  # require 'rack-cas/session_store/mongoid'
+  require 'rack-cas/session_store/mongoid'
 
   extra_attributes = [:cn, :eduPersonNickname, :sn, :title, :department, :mail, :url, :eduPersonAffiliation, :eduPersonEntitlement, :employeeId]
   Directory::Application.config.middleware.use Rack::CAS,
     server_url: Settings.cas.url,
-    # session_store: RackCAS::MongoidStore, #TODO: Having problems with Mongoid session store
+    session_store: RackCAS::MongoidStore,
     extra_attributes_filter: extra_attributes
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
-# Still having problems with mongoid and rack-cas session store
-# require 'rack-cas/session_store/rails/mongoid'
-# Directory::Application.config.session_store :rack_cas_mongoid_store
+require 'rack-cas/session_store/rails/mongoid'
+Directory::Application.config.session_store :rack_cas_mongoid_store, key: '_web_directory_session'
