66193a4778d73369c5e92173c0f880601cf557d7
Ryan Hall <ryan.hall@biola.edu>
2014-11-13 15:44:47 -0800

    Reorganizing a lot of the styles and layouts. Fixed some layout bugs.

--- app/assets/javascripts/resources/biola_header.js
+++
@@ -1,5 +0,0 @@
-$('#biolaHeader .biola-header-more > button').on('click', function(){
-  $(this).parent().toggleClass('active');
-  $('#biolaHeader').toggleClass('active');
-  $('#biolaHeader .biola-header-nav').toggleClass('active');
-});
