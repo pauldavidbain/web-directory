c94c0c81a0ef2ea940f668d73d02251f30134dbb
Ryan Hall <ryan.hall@biola.edu>
2014-12-15 16:15:20 -0800

    Adding a faculty page that will list all faculty with a bio_edition.

---
+++ app/controllers/faculty_controller.rb
@@ -0,0 +1,33 @@
+class FacultyController < ApplicationController
+
+  skip_after_action :verify_authorized, :verify_policy_scoped
+
+  def index
+    results = Elasticsearch::Model.client.search({
+      index: Settings.elasticsearch.index_name,
+      body: {
+        sort: [
+          { "last_name" => { order: :asc }},
+          { "normalized_data.title" => { order: :asc }},
+        ],
+        query: {
+          filtered: {
+            query: { match_all: {} },
+            filter: { :and => [
+              { term: { affiliations: "faculty" }},
+              { term: { is_public: true }}
+            ]}
+          }
+        }
+      }
+    })
+
+    @grouped_results = {}
+
+    results["hits"]["hits"].each do |r|
+      key = r["_source"]["last_name"].first
+      @grouped_results[key] ||= []
+      @grouped_results[key] << r
+    end
+  end
+end
