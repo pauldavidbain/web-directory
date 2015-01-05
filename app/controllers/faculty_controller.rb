5af674937db1d5677522c6830a34cbbe279f1409
Ryan Hall <ryan.hall@biola.edu>
2015-01-05 14:40:51 -0800

    Adding support for a person to be in multiple departments.

--- app/controllers/faculty_controller.rb
+++ app/controllers/faculty_controller.rb
@@ -27,9 +27,16 @@ class FacultyController < ApplicationController
     @grouped_results = {}
 
     results["hits"]["hits"].each do |r|
-      key = key_for_filter(r)
-      @grouped_results[key] ||= []
-      @grouped_results[key] << r
+      keys = Array(key_for_filter(r))
+
+      # Group results by either last name or department.
+      #  Doing it this way allows a person to be in mutiple groups (specifically departments).
+      keys.each do |key|
+        if key.present?
+          @grouped_results[key] ||= []
+          @grouped_results[key] << r
+        end
+      end
     end
 
     @keys = @grouped_results.keys.compact.sort
@@ -38,11 +45,17 @@ class FacultyController < ApplicationController
 
   private
 
+  # Return the key or keys for the given result based on the selected filter parameter.
+  # For example, if the "department" filter is selected, and the person "r" has 2 departments,
+  #   this method will return an array of those 2 departments.
+  # If the "last_name" filter is selected, it will return the first letter of the person's last name.
   def key_for_filter(r)
     case params[:filter]
     when 'department'
-      key = r["_source"]["department"]
-      key.gsub(/department( of )?/i, '').gsub(/.*School of/i, '').strip if key.present?
+      keys = r["_source"]["departments"]
+      Array(keys).map do |key|
+        key.gsub(/department( of )?/i, '').gsub(/.*School of/i, '').strip if key.present?
+      end.compact
     else
       r["_source"]["last_name"].first
     end
