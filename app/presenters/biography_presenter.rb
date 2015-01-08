06c74c4cb10dee150546cb46e7fd374efd05e8bc
Ryan Hall <ryan.hall@biola.edu>
2015-01-08 11:37:01 -0800

    Moving this to biography_presenter instead of hard coding the format into application_presenter.

--- app/presenters/biography_presenter.rb
+++ app/presenters/biography_presenter.rb
@@ -4,4 +4,8 @@ class BiographyPresenter < ApplicationPresenter
     send type, "Books", object.books.map{|b| context.format_book(b) }.join(' ')
   end
 
+  def attachment(type)
+    send type, "Documents", context.link_to("CV Document (PDF)", object.attachment.attachment.url)
+  end
+
 end
