fb2027cbcb147719b304b3bb88582fc148d20130
Ryan Hall <ryan.hall@biola.edu>
2015-01-23 13:48:57 -0800

    Fix that attachment error that is coming through on directory.

--- app/presenters/biography_presenter.rb
+++ app/presenters/biography_presenter.rb
@@ -1,11 +1,13 @@
 class BiographyPresenter < ApplicationPresenter
 
   def books(type)
-    send type, "Books", object.books.map{|b| context.format_book(b) }.join(' ')
+    send type, "Books", Array(object.books).map{|b| context.format_book(b) }.join(' ')
   end
 
   def attachment(type)
-    send type, "Documents", context.link_to("CV Document (PDF)", object.attachment.attachment.url)
+    if object.attachment.present?
+      send type, "Documents", context.link_to("CV Document (PDF)", object.attachment.attachment.url)
+    end
   end
 
 end
